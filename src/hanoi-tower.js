0ae68e96d4705f4c030b66498cbf68ab45d79234
Olga2157 <79754518+Olga2157@users.noreply.github.com>
2021-05-13 19:04:01 +0300

    Update hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -5,14 +5,16 @@ module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   let seconds = 0;
   let res = new Object();
   turns = Math.pow(2, disksNumber) - 1;
+//  Formula was used from: https://en.wikipedia.org/wiki/Tower_of_Hanoi
+//   The minimal number of moves required to solve a Tower of Hanoi puzzle is 2n − 1, where n is the number of disks.
   seconds = Math.floor((turns / turnsSpeed) * 3600);
+//   записываем результаты в свойства объекта:
   res.turns = turns;
   res.seconds = seconds;
   return res;
 };
 
 // our task is much easier than coming up with an algorithm that solves this puzzle :)
-
 // Implement the function calculateHanoi that accepts diskNumber and turnsSpeed parameters.
 // diskNumber is a number of disks and turnsSpeed is the speed of moving discs (in turns per hour). Both parameters are numbers.
 
